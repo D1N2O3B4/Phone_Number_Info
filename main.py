3fc2d024b03293e65e9964f57f31a37ac5e4803d
d1n2o3b4 <davebostons2017@gmail.com>
2023-12-28 23:55:27 +0000

    made changes

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ obj = parse(number, None)
 print(obj)
 
 num_description = geocoder.description_for_number(obj,"en")
-print(f"Number Description")
+print(f"Number Description: {num_description}")
 
 carrier_name = carrier.name_for_number(obj,"en")
 print(f"Phone Carrier: {carrier_name}")
 
-print("Timezone")
-print(timezone.time_zones_for_number(obj))
+number_timezone = timezone.time_zones_for_number(obj)
+print(f"Timezone: {number_timezone}")
